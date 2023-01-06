5b0ea0c36d1af443eeb2f42c4511c6a0dcccdf68
Shivam Gupta <shivamrvgupta@gmail.com>
2023-01-06 12:43:07 +0530

    landing section

--- js/main.js
+++ js/main.js
@@ -147,3 +147,37 @@ $(document).ready(function () {
 
 });
 
+
+
+
+// FAQ
+
+let toggles = document.getElementsByClassName('faq-wrapper');
+let contentDiv = document.getElementsByClassName('faq-content');
+let icons = document.getElementsByClassName('bx');
+
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+        if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            toggles[i].style.color = "#23daab";
+            icons[i].classList.remove('bx-plus');
+            icons[i].classList.add('bx-minus');
+        }
+        else{
+            contentDiv[i].style.height = "0px";
+            toggles[i].style.color = "#111130";
+            icons[i].classList.remove('bx-minus');
+            icons[i].classList.add('bx-plus');
+        }
+
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#111130";
+                icons[j].classList.remove('bx-minus');
+                icons[j].classList.add('bx-plus');
+            }
+        }
+    });
+}
